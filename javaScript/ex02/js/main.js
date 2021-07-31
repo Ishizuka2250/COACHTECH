9da0f51b6eee222762d38796e9d058a7e57f6b95
Ishizuka2250 <squid1028@gmail.com>
2021-07-31 11:58:30 +0900

    2021/07/30 演習実施分 part3

---
+++ javaScript/ex02/js/main.js
@@ -0,0 +1,20 @@
+// let str = "hogehoge";
+// let int = 4;
+// let dbl = 2.1;
+// let bool = true;
+// let nil = "a";
+// console.log(typeof nil);
+// let und;
+// nil = null;
+// console.log(typeof nil);
+
+// console.log(typeof str);
+// console.log(typeof int);
+// console.log(typeof dbl);
+// console.log(typeof und);
+// console.log(typeof bool);
+
+console.log(null == "");
+console.log(null == 0);
+console.log(null == undefined);
+console.log(null === undefined);
