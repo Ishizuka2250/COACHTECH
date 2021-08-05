2fa29322677e29b78b0df201c2ddb97e311d2a0a
Ishizuka2250 <squid1028@gmail.com>
2021-08-05 23:08:02 +0900

    2021/08/04 演習実施分

---
+++ javaScript/ex12/js/main.js
@@ -0,0 +1,8 @@
+const date = dayjs().format("YYYY/MM/DD HH:mm:ss");
+console.log(date);
+
+const dt = new Date();  
+year = dt.getFullYear();
+console.log(year);
+//console.log(dt.format("YYYY/MM/DD HH:mm:ss"));
+
